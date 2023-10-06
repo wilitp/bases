3181c3346c974edbcae4d858496212241280488f
wilitp <guilledeipola@gmail.com>
2023-10-05 21:08:33 -0300

    practico 6 sin el ultimo ej

--- practico6/Queries.sql
+++ practico6/Queries.sql
@@ -46,6 +46,48 @@ BEGIN
 END//
 delimiter ;
 
+-- 5. Cree una vista "Premium Customers" que devuelva el top 10 de clientes que más dinero han gastado en la plataforma. La vista deberá devolver el nombre del cliente, la ciudad y el total gastado por ese cliente en la plataforma.
+
+CREATE VIEW premium_customers AS (
+    SELECT c.customerName, o.city, sum(p.amount) AS `Total gastado` FROM customers c 
+    INNER JOIN payments p ON p.customerNumber = c.customerNumber
+    INNER JOIN employees e ON c.salesRepEmployeeNumber = e.employeeNumber
+    INNER JOIN offices o   ON o.officeCode = e.officeCode
+    GROUP BY c.customerNumber
+    ORDER BY `Total gastado` DESC
+    LIMIT 10
+)
 
 
+-- 6. Cree una función "employee of the month" que tome un mes y un año y devuelve el empleado (nombre y apellido) cuyos clientes hayan efectuado la mayor cantidad de órdenes en ese mes.
+
+delimiter //
+CREATE FUNCTION employee_of_the_month(yr int, mnth int)
+RETURNS varchar(256)
+DETERMINISTIC 
+BEGIN
+    DECLARE employee varchar(256);
+    SELECT concat(e.firstName, " ", e.lastName) INTO employee
+    FROM employees e
+    INNER JOIN customers c ON
+    c.salesRepEmployeeNumber = e.employeeNumber
+    INNER JOIN orders o ON
+    o.customerNumber = c.customerNumber 
+    WHERE MONTH(o.orderDate) = mnth AND YEAR (o.orderDate) = yr
+    GROUP BY e.employeeNumber
+    ORDER BY count(DISTINCT o.orderNumber) DESC
+    LIMIT 1;
+    RETURN employee;
+END//
+delimiter ;
+
+
+SELECT employee_of_the_month() FROM [()]
+
+SELECT a, b, c, d
+FROM (
+    SELECT 1 AS a, 2 AS b, 3 AS c, 4 AS d
+    UNION ALL 
+    SELECT 5 , 6, 7, 8
+) AS temp;
 
